3decdb55d5a4e4dad7315dcf007552736f3ffe73
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-04 20:54:49 +0100

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -21,14 +21,14 @@ void numberGen (int N, int max, int *store)
 int compare (int *A, int size){
 
 	if(size == 1){
-		return *A[0];
+		return A[0];
 	}
 	int *Out = new int[ceil(size/2)];
 	if(length%2 == 1){
 		*Out[ceil(size/2)-1] = *A[size-1];
 	}
 	for( i = 0; i < floor(length/2); i++){
-		*Out[i*2] = *A[i*2] > *A[(i*2)+1] ? *A[i*2] : *A[(i*2)+1];
+		*Out[i*2] = A[i*2] > A[(i*2)+1] ? A[i*2] : A[(i*2)+1];
 	}
 	
 	return compare(*Out,ceil(size/2));
