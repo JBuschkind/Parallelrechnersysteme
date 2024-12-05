46c90a7eb86947ce8ae4272989872fbcc0e66f21
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-05 17:12:28 +0100

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -18,22 +18,6 @@ void numberGen (int N, int max, int *store)
     store[i] = rand () % max;
 }
 
-//--------------------------------------------------------
-int compare (int *A, int size){
-
-	if(size == 1){
-		return A[0];
-	}
-	int Out = new int[(int)ceil(size/2)];
-	if(size%2 == 1){
-		Out[(int)ceil(size/2)-1] = *A[size-1];
-	}
-	for( i = 0; i < floor(size/2); i++){
-		Out[i*2] = A[i*2] > A[(i*2)+1] ? A[i*2] : A[(i*2)+1];
-	}
-	
-	return compare(*Out,ceil(size/2));
-}
 
 int main (int argc, char *argv[]){
 
@@ -44,6 +28,27 @@ int main (int argc, char *argv[]){
         cout << *A[i] << " ";
     cout << endl;
   numberGen(size, 100, A);
-  int max = compare(*A, size);
+  
+  int global_max = INT_MIN;
+  
+  #pragma omp parallel
+    {
+        int local_max = INT_MIN;
+
+        #pragma omp for
+        for (int i = 0; i < N; ++i) {
+            if (arr[i] > local_max) {
+                local_max = arr[i];
+            }
+        }
+
+        #pragma omp critical
+        {
+            if (local_max > global_max) {
+                global_max = local_max;
+            }
+        }
+    }
+	cout << "Der maximale Wert im Array ist: " << global_max << endl;
   return 0;
 }
