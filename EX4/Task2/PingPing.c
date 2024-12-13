83dcab9c780d697ecd879e73be884d33eff5c5fe
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-13 13:05:30 +0100

    Update PingPing.c

--- EX4/Task2/PingPing.c
+++ EX4/Task2/PingPing.c
@@ -4,8 +4,8 @@
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv); // MPI-Umgebung initialisieren
 
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Prozess-Rang (ID)
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Prozess-Rang (ID)
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Anzahl der Prozesse
 
