46bc4fde194d251c8e888354e8bee41ff82a2d92
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-13 13:15:32 +0100

    did things

--- EX4/Task2/PingPing.c
+++ EX4/Task2/PingPing.c
@@ -10,22 +10,49 @@ int main(int argc, char** argv) {
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Anzahl der Prozesse
 
+    // Case for Coordinator
     if (rank == 0) {
+
+        //Message with Length
         char mess[] = "Hello World";
         int len = strlen(mess)+1;
+
+        //Getting the Time at Send
         float time1 = MPI_Wtime();
+
+        //Sending the Message
         MPI_Send(mess, len, MPI_CHAR, 1, 2, MPI_COMM_WORLD);
+
+        //Preparing to recieve
         char recv[12];
         MPI_Status status;
+
+        //Recieving Message
         MPI_Recv(recv, 12, MPI_CHAR, 1, 2, MPI_COMM_WORLD, &status);
+
+        //Gatting Time after Recieving
         float time2 = MPI_Wtime();
+
+        //Calculating the time it took to PingPong
         float diff = time2 - time1;
+
+        //Output
         printf("Recieved Message in (two Way) Time: %f \n", diff);
+
+    //Case for PingPongPartner
     } else {
+
+        //Preparing to recieve
         char recv[12];
         MPI_Status status;
+
+        //Recieving Message
         MPI_Recv(recv, 12, MPI_CHAR, 0, 2, MPI_COMM_WORLD, &status);
+
+        //Preparing to send
         int len = strlen(recv)+1;
+
+        //Sending Message
         MPI_Send(recv, len, MPI_CHAR, 0, 2, MPI_COMM_WORLD);
     }
 
