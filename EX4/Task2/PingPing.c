acf93a2ff7f8073f4cc3a66d2f7c3ff8d1a628b7
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-13 12:58:49 +0100

    pingpoing your opinion is wrong

---
+++ EX4/Task2/PingPing.c
@@ -0,0 +1,33 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv); // MPI-Umgebung initialisieren
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // Prozess-Rang (ID)
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size); // Anzahl der Prozesse
+
+    if (rank == 0) [
+        char mess[] = "Hello World";
+        int len = strlen(mess)+1;
+        int time1 = MPI_Wtime();
+        MPI_Send(mess, len, MPI_CHAR, 1, MESSTAG, MPI_COMM_WORLD);
+        char recv[MAXLEN];
+        MPI_Status status;
+        MPI_Recv(recv, MAXLEN, MPI_CHAR, 1, MESSTAG, MPI_COMM_WORLD, &status);
+        int time2 = MPI_Wtime();
+        int diff = time2 - time1;
+        printf("Recieved Message in (one Way) Time: %i", diff);
+    ] else {
+        char recv[MAXLEN];
+        MPI_Status status;
+        MPI_Recv(recv, MAXLEN, MPI_CHAR, 0, MESSTAG, MPI_COMM_WORLD, &status);
+        int len = strlen(recv)+1;
+        MPI_Send(recv, len, MPI_CHAR, 0, MESSTAG, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize(); // MPI-Umgebung schließen
+    return 0;
+}
