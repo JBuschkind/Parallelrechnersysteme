4eddbc8e54bf6f863dea1153ca1f0ffa17352ce7
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-13 13:09:36 +0100

    Update PingPing.c

--- EX4/Task2/PingPing.c
+++ EX4/Task2/PingPing.c
@@ -13,14 +13,14 @@ int main(int argc, char** argv) {
     if (rank == 0) {
         char mess[] = "Hello World";
         int len = strlen(mess)+1;
-        int time1 = MPI_Wtime();
+        float time1 = MPI_Wtime();
         MPI_Send(mess, len, MPI_CHAR, 1, 2, MPI_COMM_WORLD);
         char recv[12];
         MPI_Status status;
         MPI_Recv(recv, 12, MPI_CHAR, 1, 2, MPI_COMM_WORLD, &status);
-        int time2 = MPI_Wtime();
-        int diff = time2 - time1;
-        printf("Recieved Message in (one Way) Time: %i", diff);
+        float time2 = MPI_Wtime();
+        float diff = time2 - time1;
+        printf("Recieved Message in (one Way) Time: %f \n", diff);
     } else {
         char recv[12];
         MPI_Status status;
