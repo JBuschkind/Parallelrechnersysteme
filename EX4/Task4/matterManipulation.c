44c53e22312bb8b25940bc3cea67d3cc9f5673ed
JBuschkind <38938743+JBuschkind@users.noreply.github.com>
2024-12-15 14:06:23 +0100

    Update matterManipulation.c

--- EX4/Task4/matterManipulation.c
+++ EX4/Task4/matterManipulation.c
@@ -20,7 +20,7 @@ void initialize_matrices (int n, int m, int max, int *store)
 
 // Hauptfunktion zur Matrixmultiplikation
 void matrix_multiply(int A[N][M], int B[M][N], int C[N][N], int start_row, int end_row) {
-    //#pragma omp parallel for collapse(2)
+    #pragma omp parallel for collapse(2)
     for (int i = start_row; i < end_row; i++) {
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < M; k++) {
